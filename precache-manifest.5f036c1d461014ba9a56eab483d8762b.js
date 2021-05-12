c6277df8280c3169a5421dc2f97923b56a95f663
Bhautik <bhautik.180410116121@gmail.com>
2021-05-12 16:58:21 +0530

    Updates

---
+++ precache-manifest.5f036c1d461014ba9a56eab483d8762b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "19d4feaf5c4c585e1910",
+    "url": "/FirstProjectes/static/css/main.971a3f6a.chunk.css"
+  },
+  {
+    "revision": "19d4feaf5c4c585e1910",
+    "url": "/FirstProjectes/static/js/main.a5ce1df5.chunk.js"
+  },
+  {
+    "revision": "b1035d1a07cc656140ab",
+    "url": "/FirstProjectes/static/js/runtime~main.6e15f7dc.js"
+  },
+  {
+    "revision": "740083bfce7efd9ad966",
+    "url": "/FirstProjectes/static/css/2.7e0562d4.chunk.css"
+  },
+  {
+    "revision": "740083bfce7efd9ad966",
+    "url": "/FirstProjectes/static/js/2.91db52de.chunk.js"
+  },
+  {
+    "revision": "e07253e92bcdb21588ae9772baf5d169",
+    "url": "/FirstProjectes/static/media/web2.e07253e9.png"
+  },
+  {
+    "revision": "371f0627ec3c4329dbbdfeb75c8cd9ab",
+    "url": "/FirstProjectes/static/media/web6.371f0627.png"
+  },
+  {
+    "revision": "29da132271596b29e312a2fa9c63d79d",
+    "url": "/FirstProjectes/static/media/web.29da1322.jpg"
+  },
+  {
+    "revision": "6fe23bebc0bbf247ca22e5f62a83a98b",
+    "url": "/FirstProjectes/static/media/web3.6fe23beb.png"
+  },
+  {
+    "revision": "67baff6eddca3aa1d95a44096bff6a95",
+    "url": "/FirstProjectes/static/media/web4.67baff6e.png"
+  },
+  {
+    "revision": "446aa7b06e79dafa46ccb3ec9b6212b5",
+    "url": "/FirstProjectes/static/media/web5.446aa7b0.jpg"
+  },
+  {
+    "revision": "77d7ba23963a4c5435d5620921941ea9",
+    "url": "/FirstProjectes/static/media/web7.77d7ba23.png"
+  },
+  {
+    "revision": "8561763b7f7b13c0d771aad9e01dc092",
+    "url": "/FirstProjectes/static/media/web9.8561763b.jpg"
+  },
+  {
+    "revision": "ee0424df8e1852fcfac2422df8465f0b",
+    "url": "/FirstProjectes/static/media/web10.ee0424df.jpg"
+  },
+  {
+    "revision": "6034963994b045318abc2afff7e006a9",
+    "url": "/FirstProjectes/index.html"
+  }
+];
